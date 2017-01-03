b9079b6e5ba2d67b7df48f6822b262d9aa53157f
Jan Niklas Richter <git@janniklasrichter.de>
2017-01-03 21:01:31 +0100

    Added script

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# gog-redeem-loader
+# Loader for the GOG redeem website
+
+The GOG redeem website (https://gog.com/redeem) is a pain to search in.
+All games are listed in random order, on multiple pages and there are no search fields or filters.
+
+This ruby script loads all game information of the redeem website and outputs them, so you can search them quickly.
+It shows the the page number, where the game is displayed on and then you can redeem it on the official page.
+
+## Prerequisites
+
+You only need to have ruby installed on your system. It was tested with ruby 2.3.1.
+Then copy the script into a location you have read and write permissions and you are good to go,
+
+## Usage
+
+First you need to go to the redeem page and once solve the captcha there.
+Then execute the script via command line:
+
+```
+ruby gog-redeem-loader.rb -c CODE
+```
+
+You need to specify at least the option "-c", as the script will need your redeem code to only load the games you are allowed to choose from.
+
+For other options run
+```
+ruby gog-redeem-loader.rb -h
+```
+
+## Examples
+
+### Search for specific title
+
+```
+$ ruby gog-redeem-loader.rb -c CODE -n Baphomet
+Page   Name
+4      Baphomets Fluch II: Die Spiegel der Finsternis 
+14     Baphomets Fluch: Der Engel des Todes 
+4      Baphomets Fluch: Der schlafende Drache 
+11     Baphomets Fluch: The Director's Cut 
+```
+
+So we know the "Baphomets Fluch" games are on pages 4, 11 and 14.
+
+### Print all games sorted alphabetically
+```
+$ ruby gog-redeem-loader.rb -c CODE
+```
+
+### Print all games sorted by price
+```
+$ ruby gog-redeem-loader.rb -c CODE -s price
+```
+
+## Contribute
+
+If you want to contribute to this script, fork this repo, do your changes and issue a pull request. Any help is appreciated. :)
---
+++ gog-redeem-loader.rb
@@ -0,0 +1,119 @@
+require "net/http"
+require "json"
+require "optparse"
+
+@key = nil
+@sort_by = :alpha
+@search_for = nil
+@force = false
+
+def parseOptions
+  OptionParser.new do |o|
+    o.on("-c", "--code CODE", "Required. Specify your redeem CODE.") {|code| @key = code}
+    o.on("-s", "--sortby SORT", "Sort method. Can be alpha or price. Defaults to alpha.") {|sort| @sort_by = sort.to_sym}
+    o.on("-n", "--name NAME", "Only print the games with NAME in their title.") {|name| @search_for = name}
+    o.on("-f", "--force", "Force loading games from server. Skips cached results.") {|force| @force = force }
+    o.on("-h", "--help", "Print this help.") {|h| puts o; exit;}
+    o.parse!
+  end
+
+  if @key.nil?
+    puts "ERRO: You must specify a redeem code."
+    exit
+  end
+end
+
+def loadGames(key)
+  games = []
+  page = 1
+  resume = true
+
+  while (resume) do
+    uri = URI("https://www.gog.com/redeem/getProducts/#{key}?page=#{page}")
+    response = Net::HTTP.get_response(uri)
+
+    if (response.code == '429')
+      puts "INFO: Hit rate limit. Sleeping for 30 seconds..."
+      sleep(30)
+      next
+    elsif (response.code == '200')
+      puts "SCCS: Fetched page #{page}"
+      games_page = JSON.parse(response.body);
+      games_page["products"].each do |game|
+        game["page"] = page
+        games.push(game)
+      end
+      page += 1
+    elsif (response.code == '403')
+      puts "ERRO: Got access denied. Please visit the redeem website once and solve the captcha."
+      resume = false
+      return nil
+    else
+      resume = false
+    end
+  end
+
+  return games
+end
+
+def cacheResults(filename, games)
+  File.open(filename, "w") do |file|
+    file.puts(Marshal.dump(games))
+  end
+end
+
+def loadCache(filename)
+  begin
+    data = File.read(filename)
+    return Marshal.load(data)
+  rescue Exception => e
+    puts "Exception #{e.inspect}"
+    return nil
+  end
+end
+
+def printGamesSorted(games, sort_method)
+  case (sort_method)
+  when :price
+    puts "Page \t Price \t\t Name"
+    puts games.sort_by{|g| g["title"]}.sort_by{|g| g["price"]["baseAmount"]}.map{|g| "#{g["page"]} \t #{g["price"]["baseAmount"]} #{g["price"]["symbol"]} \t #{g["title"]} \n"}
+  else
+    puts "Page \t Name"
+    puts games.sort_by{|g| g["title"]}.map{|g| "#{g["page"]} \t #{g["title"]} \n"}
+  end
+end
+
+def printGamesSearch(games, search_string)
+  games = games.select{|g| g["title"].downcase.include?(search_string.downcase)}
+  printGamesSorted(games, :alpha)
+end
+
+def main
+  parseOptions
+
+  games = nil
+
+  # load from cache
+  games = loadCache("games.cache-"+@key) unless @force
+  loaded_by_cache = true
+
+  # load from server
+  if games.nil?
+    puts "INFO: Load games from server."
+    games = loadGames(@key)
+    loaded_by_cache = false
+  end
+
+  exit if games.nil?
+  cacheResults("games.cache-"+@key, games) unless loaded_by_cache
+
+  # output
+  puts "INFO: Results:" unless loaded_by_cache
+  unless @search_for.nil?
+    printGamesSearch(games, @search_for)
+  else
+    printGamesSorted(games, @sort_by)
+  end
+end
+
+main
